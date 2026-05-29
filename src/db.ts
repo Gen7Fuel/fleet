176e5d0d196d9493d7f0d5e31c74300ebd9277d5
Mohammad Khan <mohammad@gen7fuel.com>
2026-05-29 09:23:25 -0400

    update mongo uri

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,13 @@
 import { MongoClient, type Db } from 'mongodb'
 
-const uri = process.env.MONGO_URI ?? 'mongodb://mongo:27017/thehub'
+const uri = process.env.MONGO_URI ?? 'mongodb://localhost:27018/main'
 const client = new MongoClient(uri)
 let db: Db | null = null
 
 export async function getDb(): Promise<Db> {
   if (!db) {
     await client.connect()
-    db = client.db('thehub')
+    db = client.db(new URL(uri).pathname.slice(1))
   }
   return db
 }
